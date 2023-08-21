42587d77847e3910df5d58f30180275735b07333
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-21 14:34:41 -0300

    :tada: first commit

---
+++ typescript-basico/annotation-e-inference/exercicio-1/script.js
@@ -0,0 +1,5 @@
+"use strict";
+// Conserte a função com TypeScript
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
+}
---
+++ typescript-basico/annotation-e-inference/exercicio-1/script.ts
@@ -0,0 +1,4 @@
+// Conserte a função com TypeScript
+function normalizarTexto(texto: string) {
+  return texto.trim().toLowerCase();
+}
