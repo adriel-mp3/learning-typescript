c4aa7363ba6122b0fe072e1a11b1882187dd332f
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-28 17:51:05 -0300

    :books: exercise(interfaces-do-DOM): solution

---
+++ objetos-e-interfaces/interfaces-do-DOM/script.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+1 - Selecione os elementos com a classe link.
+2 - Crie uma função que deve ser executada para cada elemento.
+3 - Modificar através da função o estilo da color e border.
+ */
+const elements = document.querySelectorAll(".link");
+elements.forEach((element) => {
+    // console.dir(element.__proto__) só funciona em browser verifica o protótipo **não usar**
+    if (element instanceof HTMLElement) {
+        handleElements(element);
+    }
+});
+function handleElements(element) {
+    element.style.borderBottom = "2px solid red";
+    element.style.color = "red";
+}
