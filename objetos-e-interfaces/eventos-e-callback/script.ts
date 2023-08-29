80ee9832f43232769d564027dedf2277b2eba57c
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-29 14:58:28 -0300

    :books: exercise(eventos-e-callback): solution

---
+++ objetos-e-interfaces/eventos-e-callback/script.ts
@@ -0,0 +1,18 @@
+// Dada a estrutura HTML/CSS, crie o script que irá fazer o botão mobile funcionar (ativar/desativar a navegação).
+
+const menuButton = document.querySelector("#btn-mobile");
+const navMenu = document.querySelector("#nav");
+
+function toggleMenu(event: Event) {
+  const navMenu = document.querySelector("#nav");
+  const isMenuActive = navMenu?.classList.contains("active");
+  const button = event.currentTarget;
+
+  if (button instanceof HTMLElement && navMenu) {  
+    navMenu.classList.toggle("active");
+    button.setAttribute("aria-expanded", String(isMenuActive));
+    button.setAttribute("aria-label", isMenuActive ? "Fechar Menu" : "Abrir Menu");
+  }
+}
+
+menuButton?.addEventListener("pointerdown", toggleMenu);
